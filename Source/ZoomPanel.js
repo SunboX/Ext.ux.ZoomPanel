7014b055f536fbc30069a0ab81c4a0b282b03d31
André Fiedler <kontakt@visualdrugs.net>
2011-10-30 12:16:17 +0000

    more work

--- Source/ZoomPanel.js
+++ Source/ZoomPanel.js
@@ -11,10 +11,48 @@ Ext.define('Ext.ux.ZoomPanel', function(){
         }
     },
     
-    construct: function(){
-        
-        
-        
+    initialize: function() {
+        this.touches = 0;
+        this.currentScale = 0.0;
+        this.element.on({
+            scope      : this,
+            //tap        : 'onTap',
+            touchstart : 'onTouchStart',
+            touchmove  : 'onTouchMove',
+            touchend   : 'onTouchEnd'
+        });
         this.callParent();
+    },
+
+    // @private
+    onTouchStart: function(e) {
+        this.touches = e.touches.length;
+    },
+
+    // @private
+    onTouchMove: function(e) {
+        e.preventDefault();
+        if(this.touches == 2){
+            this.doZoom(e.scale);
+        }
+    },
+
+    // @private
+    onTouchEnd: function(e) {
+        if(this.touches == 2){
+            this.doZoom(e.scale);
+        }
+        this.touches = e.touches.length;
+    },
+
+    // @private
+    doZoom: function(e) {
+        this.fireEvent('beforeZoom', [this, 100.0 * this.currentScale]));
+        this.currentScale += e.scale;
+        if(this.currentScale < 0)
+            this.currentScale = 0.0;
+        var zoom = 100.0 * this.currentScale;
+        this.element.setStyle('zoom', zoom + '%');
+        this.fireEvent('zoom', [this, zoom]));
     }
 });
